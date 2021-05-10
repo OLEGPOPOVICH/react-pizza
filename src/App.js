ebb96287058f3ea40a55d87af7c13a2269e51f1a
Попович Олег Игоревич <popovich.oi@esphere.ru>
2021-05-10 15:23:16 +0300

    removed button sentry

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-/* eslint-disable react/button-has-type */
 import { CommonLoyout } from './common/Loyout';
 import { PizzaConstructorPage } from './pages/PizzaConstructorPage/PizzaConstructorPage';
 
@@ -6,14 +5,6 @@ function App() {
   return (
     <CommonLoyout>
       <PizzaConstructorPage />
-      <button
-        onClick={() => {
-          throw new Error('Something went wron');
-        }}
-      >
-        Break the world
-      </button>
-      ;
     </CommonLoyout>
   );
 }
